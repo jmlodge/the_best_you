4390793e3a3c7b3976277cfaf70f9a284f14eab6
jmlodge <jm-lodge@hotmail.co.uk>
2017-04-12 19:40:12 +0100

    adds base and starts log in procedure

--- accounts/views.py
+++ accounts/views.py
@@ -1,8 +1,12 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from accounts.forms import UserForm, UserProfileForm
+from django.contrib import auth
+from django.contrib.auth import authenticate
+from django.core.urlresolvers import reverse
+from django.template.context_processors import csrf
 from django.contrib import messages
 
 
@@ -40,4 +44,27 @@ def register(request):
 
     return render(request,
                   'accounts/register.html',
-                  {'user_form': user_form, 'user_profile_form': user_profile_form,'registered': registered})
+                  {'user_form': user_form, 'user_profile_form': user_profile_form, 'registered': registered})
+
+
+def login(request):
+    if request.method == 'POST':
+        # need UserLoginForm
+        form = UserLoginForm(request.POST)
+        if form.is_valid():
+            user = authenticate(username=request.POST.get('username'),
+                                password=request.POST.get('password'))
+
+            if user is not None:
+                auth.login(request, user)
+                return redirect(reverse('profile'))
+            else:
+                form.add_error(None, "Your Username and, or Password was not recognised")
+
+        else:
+            form = UserLoginForm()
+
+        args = {'form': form}
+        args.update(csrf(request))
+        # make login.html
+        return render(request, 'login.html', args)
--- the_best_you/urls.py
+++ the_best_you/urls.py
@@ -1,8 +1,10 @@
 from django.conf.urls import url
 from django.contrib import admin
 from accounts import views as account_views
+from home import views as home_views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^$', home_views.home, name='home'),
     url(r'^register/$', account_views.register, name='register'),
 ]
